646d2f04af4f3fde38b2c5694f9fd98e0a2e2e96
Nic <nic.smith@immersve.com>
2022-04-20 17:10:21 +1200

    Move page out of Footer

---
+++ src/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    colors: {
+      immersvepink: "#FF15F4",
+    },
+    fontFamily: {
+      bebasneue: [`"Bebas Neue"`],
+      montserrat: [`"Montserrat"`],
+    },
+  },
+};
