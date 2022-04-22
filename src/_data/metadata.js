dd19cf64b2b9bc141f7b1af8d1d2cbb616e0b503
Nic <nic.smith@immersve.com>
2022-04-22 17:00:45 +1200

    Fix metadata

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -7,4 +7,7 @@ const baseUrl = config.get('baseUrl');
 module.exports = {
   title: 'Immersve',
   url: `${baseUrl}/`,
+  description: 'World\'s first multichain protocol',
+  site_name: 'Immersve',
+  // img: `${baseUrl}/img/`,
 };
